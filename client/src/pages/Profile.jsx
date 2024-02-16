6f2c3464ef27bcf6414ae7362ba07c96d624ca1f
Sarthak Jain <jainsarthak2003@gmail.com>
2024-02-16 13:18:55 +0530

    fixed responsiveness at profile pg dp

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,16 +17,16 @@ const Profile = () => {
         </div>
       </div>
       <div className="relative">
-        <div className="absolute -top-20 lg:-top-32 left-20 xl:left-28 overflow-hidden rounded-full size-44 md:size-40 lg:size-52 xl:size-60">
+        <div className="absolute -top-16 sm:-top-20 lg:-top-32 left-6 sm:left-14 xl:left-28 overflow-hidden rounded-full size-32 sm:size-40 lg:size-52 xl:size-60">
           <img src="/rnd.jpg" alt="profile-img" className="w-full object-cover" />
         </div>
       </div>
       <div className="flex flex-col md:flex-row">
-        <div className="flex flex-col justify-center items-center pt-40 xl:w-[37%] bg-[#d9d9d9]/40 gap-16">
+        <div className="flex flex-col justify-center items-center pt-40 md:w-[37%] bg-[#d9d9d9]/40 gap-16 px-8">
           <div className="flex flex-col justify-center items-center gap-8">
             <div className="flex gap-1 items-center">
               <img src="/location-icon.svg" alt="" />
-              <p className="text-xl font-poppins">Bangalore, Karnataka</p>
+              <p className="text-base lg:text-xl font-poppins">Bangalore, Karnataka</p>
             </div>
             <div className="flex justify-between items-center w-full">
               <img src="/github.svg" alt="github-icon" />
@@ -39,14 +39,14 @@ const Profile = () => {
           </div>
         </div>
 
-        <div className="flex flex-col px-8 py-6 bg-primaryBackground xl:w-[63%]">
-          <div className="flex gap-8 items-center">
-            <p className="font-poppins text-4xl">Kshitiz Sachan</p>
+        <div className="flex flex-col px-8 py-6 bg-primaryBackground md:w-[63%]">
+          <div className="flex flex-col lg:flex-row gap-5 lg:gap-8 lg:items-center">
+            <p className="font-poppins text-3xl xl:text-4xl">Kshitiz Sachan</p>
             <div className="flex gap-3">
-              <div className="px-7 py-2 bg-[#F5DEE7] border-2 border-[#FA005E] rounded-3xl text-sm font-bold">
+              <div className="px-5 sm:px-7 py-2 bg-[#F5DEE7] border-2 border-[#FA005E] rounded-3xl text-xs sm:text-sm font-bold h-fit">
                 Alumni
               </div>
-              <div className="px-7 py-2 bg-[#D9EFD3] border-2 border-[#3ACC16] rounded-3xl text-sm font-bold">
+              <div className="px-5 sm:px-7 py-2 bg-[#D9EFD3] border-2 border-[#3ACC16] rounded-3xl text-xs sm:text-sm font-bold text-center h-fit">
                 Open for Mentorship
               </div>
             </div>
