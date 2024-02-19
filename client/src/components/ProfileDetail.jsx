e7f2cf8bfcc396412ef96c26766239d0869780c1
Sarthak Jain <jainsarthak2003@gmail.com>
2024-02-20 01:13:53 +0530

    adding edit profile form and copy to clipboard

--- client/src/components/ProfileDetail.jsx
+++ client/src/components/ProfileDetail.jsx
@@ -1,15 +1,99 @@
-import React from "react";
+import { PrimaryButton } from "./Buttons";
+import { Bounce, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const ProfileDetail = ({ type, detail }) => {
+const Detail = ({ type, detail }) => {
+  const handleCopyClick = (text) => {
+    navigator.clipboard.writeText(text);
+    toast.info("Copied to clipboard", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+      toastId: "copied",
+    });
+  };
+
   return (
-    <div className="flex justify-between bg-white border border-[#8a8a8a] rounded-xl px-3 py-2 sm:w-80 font-poppins gap-4">
-      <div className="flex flex-col">
-        <p className="text-xs 2xl:text-sm text-black/50">{type}</p>
-        <p className="text-sm 2xl:text-base">{detail}</p>
+    <>
+      <div className="flex justify-between bg-white border border-[#8a8a8a] rounded-xl px-3 py-2 sm:w-80 font-poppins gap-4">
+        <div className="flex flex-col">
+          <p className="text-xs 2xl:text-sm text-black/50">{type}</p>
+          <p className="text-sm 2xl:text-base">{detail}</p>
+        </div>
+        <img
+          src="/copy-icon.svg"
+          alt="copy-icon"
+          className="cursor-pointer"
+          onClick={() => handleCopyClick(detail)}
+        />
       </div>
-      <img src="/copy-icon.svg" alt="copy-icon" />
-    </div>
+    </>
   );
 };
 
-export default ProfileDetail;
+const EditDetails = ({ handleEdit, handleFormClose, data }) => {
+
+  const handleSaveClick = async () => {
+    // api call to update details with data._id
+    toast.success("Saved successfully", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+      toastId: "editsaved",
+    });
+    handleFormClose()
+  }
+
+  return (
+    <>
+      <div className="flex flex-col gap-8 px-10 py-12">
+        <div className="flex flex-col md:flex-row gap-6 md:gap-10 justify-between items-center">
+          <div className="flex flex-col gap-1">
+            <p className="font-poppins text-black/70">Position</p>
+            <input type="text" value={data.position} onChange={e => handleEdit("position", e)} className="py-2 px-3 border border-black/50 rounded-lg md:w-80" />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <p className="font-poppins text-black/70">Company</p>
+            <input type="text" value={data.company} onChange={e => handleEdit("company", e)} className="py-2 px-3 border border-black/50 rounded-lg md:w-80" />
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row gap-6 md:gap-10 justify-between items-center">
+          <div className="flex flex-col gap-1">
+            <p className="font-poppins text-black/70">Email</p>
+            <input type="text" value={data.email} onChange={e => handleEdit("email", e)} className="py-2 px-3 border border-black/50 rounded-lg md:w-80" />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <p className="font-poppins text-black/70">Batch</p>
+            <input type="text" value={data.batch} onChange={e => handleEdit("batch", e)} className="py-2 px-3 border border-black/50 rounded-lg md:w-80" />
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row gap-6 md:gap-10 justify-between items-center md:items-end">
+          <div className="flex flex-col gap-1">
+            <p className="font-poppins text-black/70">Branch</p>
+            <input type="text" value={data.branch} onChange={e => handleEdit("branch", e)} className="py-2 px-3 border border-black/50 rounded-lg md:w-80" />
+          </div>
+
+          <div onClick={handleSaveClick}>
+            <PrimaryButton name={"SAVE"} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Detail, EditDetails };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Navbar from "../template/Navbar";
 import { BorderButton } from "../components/Buttons";
-import ProfileDetail from "../components/ProfileDetail";
+import { Detail, EditDetails } from "../components/ProfileDetail";
 import Notification from "../components/Notification";
 import Footer from "../template/Footer";
+import { Bounce, ToastContainer } from "react-toastify";
 
 const sample = {
+  _id: 223123213,
   position: "Software Engineer",
   company: "Walmart",
   email: "johndoe@hotmail.com",
@@ -16,10 +19,30 @@ const sampleNotifications = [
   {id: 1, type: "REFERRAL_REQUEST", name: "Priyal", project: "", position: "SDE", company: "IIIT Dharwad", actionTaken: ""},
   {id: 2, type: "PROJECT_COLLAB_REQUEST", name: "Priyal", project: "mini project", position: "", company: "", actionTaken: ""},
   {id: 3, type: "PROJECT_COLLAB_RESPONSE", name: "Priyal", project: "mini project", position: "", company: "", actionTaken: "rejected"},
-  {id: 4, type: "REFERRAL_RESPONSE", name: "Priyal", project: "", position: "SDE", company: "IIIT Dharwad", actionTaken: "accepted"}
+  {id: 4, type: "REFERRAL_RESPONSE", name: "Priyal", project: "", position: "SDE", company: "IIIT Dharwad", actionTaken: "accepted"},
+  {id: 5, type: "REFERRAL_REQUEST", name: "Priyal", project: "", position: "SDE", company: "IIIT Dharwad", actionTaken: ""},
+  {id: 6, type: "PROJECT_COLLAB_REQUEST", name: "Priyal", project: "mini project", position: "", company: "", actionTaken: ""},
+  {id: 7, type: "PROJECT_COLLAB_RESPONSE", name: "Priyal", project: "mini project", position: "", company: "", actionTaken: "rejected"},
+  {id: 8, type: "REFERRAL_RESPONSE", name: "Priyal", project: "", position: "SDE", company: "IIIT Dharwad", actionTaken: "accepted"}
 ]
 
 const Profile = () => {
+  const [data, setData] = useState(sample)
+
+  const handleDetailChange = (title, event) => {
+    setData({...data, [title]: event.target.value})
+  }
+
+  const handleEditFormOpen = () => {
+    const modal = document.getElementById("edit-form")
+    modal.showModal()
+  }
+
+  const handleEditFormClose = () => {
+    const modal = document.getElementById("edit-form")
+    modal.close()
+  }
+
   return (
     <>
       <Navbar />
@@ -31,13 +54,13 @@ const Profile = () => {
             className="w-full object-cover relative top-0 md:-top-[6em] -z-[1]"
           />
         </div>
-        <div className="absolute bottom-0 right-3">
+        <div className="absolute bottom-0 right-3" onClick={handleEditFormOpen}>
           <BorderButton name={"Edit Profile"} />
         </div>
       </div>
 
       <div className="flex flex-col md:flex-row">
-        <div className="relative flex flex-col items-center md:w-[37%] lg:w-[33%] bg-[#d9d9d9]/40 gap-16 px-8">
+        <div className="relative flex flex-col items-center md:w-[37%] lg:w-[30%] bg-[#d9d9d9]/40 gap-16 px-8">
           <div className="absolute -top-16 sm:-top-20 lg:-top-28 xl:-top-32 left-8 sm:left-auto overflow-hidden rounded-full size-32 sm:size-40 lg:size-52 xl:size-60">
             <img
               src="/rnd.jpg"
@@ -54,15 +77,21 @@ const Profile = () => {
               </p>
             </div>
             <div className="flex justify-between items-center w-full">
-              <img src="/github.svg" alt="github-icon" />
-              <img src="/twittor.svg" alt="twittor-icon" />
-              <img src="/linkedin.svg" alt="linkedin-icon" />
+              <a href="https://github.com/" target="blank">
+                <img src="/github.svg" alt="github-icon" />
+              </a>
+              <a href="https://github.com/" target="blank">
+                <img src="/twittor.svg" alt="twittor-icon" />
+              </a>
+              <a href="https://github.com/" target="blank">
+                <img src="/linkedin.svg" alt="linkedin-icon" />
+              </a>
             </div>
           </div>
           <div className="">{/* Projects floated section */}</div>
         </div>
 
-        <div className="flex flex-col gap-14 px-8 pt-6 pb-40 bg-primaryBackground md:w-[63%] lg:w-[67%]">
+        <div className="flex flex-col gap-14 px-8 pt-6 pb-40 bg-primaryBackground md:w-[63%] lg:w-[70%]">
           <div className="flex flex-col lg:flex-row gap-5 lg:gap-8 lg:items-center">
             <p className="font-poppins text-3xl xl:text-4xl">Kshitiz Sachan</p>
             <div className="flex gap-3">
@@ -76,18 +105,18 @@ const Profile = () => {
           </div>
 
           {/* Details and notifications */}
-          <div className="flex flex-col xl:flex-row gap-10 xl:gap-6 2xl:gap-14">
+          <div className="flex flex-col xl:flex-row gap-10 xl:gap-5 2xl:gap-14">
             <div className="flex flex-col gap-3">
-              <ProfileDetail type={"Position"} detail={sample.position} />
-              <ProfileDetail type={"Company"} detail={sample.company} />
-              <ProfileDetail type={"Email"} detail={sample.email} />
-              <ProfileDetail type={"Batch"} detail={sample.batch} />
-              <ProfileDetail type={"Branch"} detail={sample.branch} />
+              <Detail type={"Position"} detail={data.position} />
+              <Detail type={"Company"} detail={data.company} />
+              <Detail type={"Email"} detail={data.email} />
+              <Detail type={"Batch"} detail={data.batch} />
+              <Detail type={"Branch"} detail={data.branch} />
             </div>
 
             <div className="border border-solid border-black/20"></div>
 
-            <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-6 h-[350px] px-3 pt-2 pb-5 overflow-y-auto w-fit">
               {sampleNotifications.map((item) => (
                 <Notification key={item.id} type={item.type} name={item.name} project={item.project} position={item.position} company={item.company} actionTaken={item.actionTaken} />
               ))}
@@ -95,7 +124,26 @@ const Profile = () => {
           </div>
         </div>
       </div>
+
+      <dialog id="edit-form" className="backdrop:bg-black/40">
+        <EditDetails data={data} handleEdit={handleDetailChange} handleFormClose={handleEditFormClose} />
+      </dialog>
+
       <Footer />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+        theme="light"
+        transition={Bounce}
+      />
     </>
   );
 };
